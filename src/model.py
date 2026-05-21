ef20d2c97b3c27c6cc73d564fafca937382a57d3
bagjongseon <whdtjs20@gmail.com>
2026-05-21 11:13:13 +0900

    [MA] get_modeL() 모델 사이즈 선택 기능 추가

--- src/model.py
+++ src/model.py
@@ -4,11 +4,13 @@
 # 2. 모델 로드
 from ultralytics import YOLO
 
-def get_model():
+def get_model(model_size="m"):
     """
-    YOLOv11 Medium 모델 반환
+    YOLOv11 모델 반환
     - num_classes: 4 (알약 종류)
     - pretrained: yolo11m.pt
+    - model_size: n, s, m, l, x 선택 가능 (기본값: m)
+    - pretrained: yolo11{model_size}.pt
     """
-    model = YOLO("yolo11m.pt")
+    model = YOLO(f"yolo11{model_size}.pt")
     return model
